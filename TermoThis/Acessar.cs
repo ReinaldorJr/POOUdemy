d12542038eb45fe18e139bbc37a18e128ffca446
ReinaldorJr <111937187+ReinaldorJr@users.noreply.github.com>
2023-04-24 15:53:34 -0300

    240423

---
+++ TermoThis/Acessar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermoThis
+{
+    internal class Acessar
+    {
+        string senha = "abc123";
+
+        public bool Login(string senha)
+        {
+            return this.senha == senha;//onde tem o this faz referencia ao campo senha da classe...
+                                       //string senha = "abc123";
+                                       //depois do == referencia public bool Login(string senha)
+                                       //por isso usamos o termo this para diferencia-los.
+        }
+    }
+}
